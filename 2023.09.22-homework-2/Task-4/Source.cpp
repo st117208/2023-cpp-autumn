aa596dec9dca5d313b5eaed98310a5d869c429ba
st117208 <st117208.student.spbu.ru>
2023-09-23 03:36:24 +0300

    homework-2 code update

--- 2023.09.22-homework-2/Task-4/Source.cpp
+++ 2023.09.22-homework-2/Task-4/Source.cpp
@@ -9,35 +9,32 @@ int main(int argc, char* argv[])
 	int t20 = 0;
 	int t60 = 0;
 	std::cin >> a;
-	if (a >= 60)
+	t60 = a / 60;
+	a = a % 60;
+	if (a >= 36)
 	{
-		t60 = a / 60;
-		a = a % 60;
-		if (a >= 36)
+		t60 = t60 + 1;
+	}
+	else
+	{
+		t20 = a / 20;
+		a = a % 20;
+		if (a >= 18)
 		{
-			t60 = t60 + 1;
+			t20 = t20 + 1;
 		}
 		else
 		{
-			t20 = a / 20;
-			a = a % 20;
-			if (a >= 18)
+			t10 = a / 10;
+			a = a % 10;
+			if (a >= 9)
 			{
-				t20 = t20 + 1;
+				t10 = t10 + 1;
 			}
 			else
 			{
-				t10 = a / 10;
-				a = a % 10;
-				if (a >= 9)
-				{
-					t10 = t10 + 1;
-				}
-				else
-				{
-					t5 = a / 5;
-					t1 = a % 5;
-				}
+				t5 = a / 5;
+				t1 = a % 5;
 			}
 		}
 	}
--- 2023.09.22-homework-2/Task-7/Source.cpp
+++ 2023.09.22-homework-2/Task-7/Source.cpp
@@ -4,10 +4,50 @@ int main(int argc, char* argv[])
 {
 	int a = 0;
 	std::cin >> a;
-	if (a == 100)
+	switch (a / 10)
 	{
-		std::cout << "C" << std::endl;
+	case 1: std::cout << "X";
+		break;
+	case 2: std::cout << "XX";
+		break;
+	case 3: std::cout << "XXX";
+		break;
+	case 4: std::cout << "XL";
+		break;
+	case 5: std::cout << "L";
+		break;
+	case 6: std::cout << "LX";
+		break;
+	case 7: std::cout << "LXX";
+		break;
+	case 8: std::cout << "LXXX";
+		break;
+	case 9: std::cout << "XC";
+		break;
+	case 10: std::cout << "c";
+		break;
 	}
-	else
+
+	switch (a % 10)
+	{
+	case 1: std::cout << "I";
+		break;
+	case 2: std::cout << "II";
+		break;
+	case 3: std::cout << "III";
+		break;
+	case 4: std::cout << "IV";
+		break;
+	case 5: std::cout << "V";
+		break;
+	case 6: std::cout << "VI";
+		break;
+	case 7: std::cout << "VII";
+		break;
+	case 8: std::cout << "VIII";
+		break;
+	case 9: std::cout << "IX";
+		break;
+	}
 	return EXIT_SUCCESS;
 }
